9f01ffa261b9971722991df7c37ffd33f007fa17
Nick <masihnishit@gmail.com>
2025-02-01 02:03:26 +0530

    delete account rest api

--- src/main/java/com/alpha/www/Banking/controller/AccountController.java
+++ src/main/java/com/alpha/www/Banking/controller/AccountController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,5 +62,11 @@ public class AccountController {
 	@GetMapping("/all")
 	public ResponseEntity<List<AccountDto>> getAllAccounts(){
 		return ResponseEntity.ok(accountService.getAllAccounts());
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteAccount(@PathVariable Long id){
+		accountService.deleteAccount(id);
+		return ResponseEntity.ok("Account successfully deleted with id: " + id);
 	}
  }
--- src/main/java/com/alpha/www/Banking/service/impl/AccountServiceImpl.java
+++ src/main/java/com/alpha/www/Banking/service/impl/AccountServiceImpl.java
@@ -81,4 +81,12 @@ public class AccountServiceImpl implements AccountService {
 				.collect(Collectors.toList());
 	}
 
+	@Override
+	public void deleteAccount(Long id) {
+		Account account = accountRepository
+				.findById(id)
+				.orElseThrow(() -> new RuntimeException("Account not found with id: " + id));
+		accountRepository.deleteById(id);
+	}
+
 }
