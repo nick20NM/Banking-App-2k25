c74941732db227ae83afe0f652298f5141aa2d18
Nick <masihnishit@gmail.com>
2025-02-01 02:26:39 +0530

    java record used

--- src/main/java/com/alpha/www/Banking/dto/AccountDto.java
+++ src/main/java/com/alpha/www/Banking/dto/AccountDto.java
@@ -1,13 +1,19 @@
 package com.alpha.www.Banking.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//public class AccountDto {
+//	private Long id;
+//	private String accountHolderName;
+//	private double balance;
+//}
 
-@Data
-@AllArgsConstructor
-public class AccountDto {
-	private Long id;
-	private String accountHolderName;
-	private double balance;
+public record AccountDto(
+		Long id, 
+		String accountHolderName, 
+		double balance) {
 }
 
--- src/main/java/com/alpha/www/Banking/mapper/AccountMapper.java
+++ src/main/java/com/alpha/www/Banking/mapper/AccountMapper.java
@@ -5,11 +5,18 @@ import com.alpha.www.Banking.entity.Account;
 
 public class AccountMapper {
 
+//	public static Account mapToAccount(AccountDto accountDto) {
+//		return new Account(
+//				accountDto.getId(), 
+//				accountDto.getAccountHolderName(), 
+//				accountDto.getBalance());
+//	}
+	
 	public static Account mapToAccount(AccountDto accountDto) {
 		return new Account(
-				accountDto.getId(), 
-				accountDto.getAccountHolderName(), 
-				accountDto.getBalance());
+				accountDto.id(), 
+				accountDto.accountHolderName(), 
+				accountDto.balance());
 	}
 	
 	public static AccountDto mapToAccountDto(Account account) {
